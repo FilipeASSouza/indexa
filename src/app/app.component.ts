50775e81103c489a50700fc16c764dee52696efa
Filipe NoteCasa <filipe.sistemas@gmail.com>
2025-10-20 19:38:08 -0300

    add: appcomponent e modificando styles padrao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, ɵEmptyOutletComponent } from '@angular/router';
+import { ContainerComponent } from "./componentes/container/container.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule,
+    RouterOutlet,
+    ɵEmptyOutletComponent,
+    ContainerComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
