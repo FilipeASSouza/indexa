d513c97a62c40797fef833e5eb0f26bd5b1e13c3
Filipe NoteCasa <filipe.sistemas@gmail.com>
2025-12-03 18:22:26 -0300

    add: componentes form tornando formulario reactivo

--- src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { ContainerComponent } from '../../componentes/container/container.component';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+import { ContainerComponent } from '../../componentes/container/container.component';
 import { SeparadorComponent } from '../../componentes/separador/separador.component';
 
 @Component({
@@ -9,11 +11,25 @@ import { SeparadorComponent } from '../../componentes/separador/separador.compon
   imports: [
     CommonModule,
     ContainerComponent,
-    SeparadorComponent
+    SeparadorComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './formulario-contato.component.html',
   styleUrl: './formulario-contato.component.css'
 })
 export class FormularioContatoComponent {
+
+  contatoForm !:FormGroup;
+
+  constructor() {
+    this.contatoForm = new FormGroup({
+      nome :new FormControl('Test'),
+      telefone :new FormControl(''),
+      email :new FormControl(''),
+      aniversario :new FormControl(''),
+      redes :new FormControl(''),
+      observacoes :new FormControl('')
+    });
+  }
 
 }
